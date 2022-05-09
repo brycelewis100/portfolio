aca3fc14fc7cbd148845c599eb9f60373ae63f6e
TRbrycelewis <72512436+TRbrycelewis@users.noreply.github.com>
2022-05-09 09:04:15 -0700

    words update

--- src/Projects/biggest-little-racing/Components/EventList.js
+++ src/Projects/biggest-little-racing/Components/EventList.js
@@ -12,8 +12,8 @@ const EventList = () => {
         description={[
           'We’re bringing free grassroots racing to Reno!',
           'Inspired by the Tears Fears and Beers Enduro organized by our friends out in Ely, my goal with this series is to get the community together to enjoy big days on the bike. In the future I’d like to organize events that incorporate camping and racing into one big weekend, but for this first event, we’ll just stick to racing.',
-          'To keep things simple, I will be using Strava to time the event, and we will be following the course map from the 2016 CES. Anyone who completes all 6 stages before I calculate the results on the evening of Mach 15th will be counted in the final results.',
-          'For those that want to roll out as a group, the main “event” will start at the softball fields on Saturday, May 15th at 9:30am. ',
+          'To keep things simple, I will be using Strava to time the event, and we will be following the course map from the 2016 CES. Anyone who completes all 6 stages before I calculate the results on the evening of March 15th will be counted in the final results.',
+          'For those that want to roll out as a group, the main “event” will start at the softball fields on Saturday, May 14th at 9:30am. ',
           'I hope to see you out there!',
         ]}
         img={'https://ep1.pinkbike.org/p5pb13495230/p5pb13495230.jpg'}
